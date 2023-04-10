d3a355d48210e043ca7a190f2e38ce6b4936f490
KingHermando <herman.noren@outlook.com>
2023-04-10 21:06:49 +0200

    marker on map test

--- react_app/src/components/map/InteractiveMap.jsx
+++ react_app/src/components/map/InteractiveMap.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./interactiveMap.css";
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 
 const InteractiveMap = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   });
 
-  if (!isLoaded) return <div>Loading...</div>;
+  const center = useMemo(() => ({ lat: 57.9, lng: 12.5 }), []);
+  const hermansHus = useMemo(
+    () => ({ lat: 57.8849039096269, lng: 12.473770972272334 }),
+    []
+  );
 
+  if (!isLoaded) return <div>Loading...</div>;
   return (
-    <GoogleMap
-      zoom={12}
-      center={{ lat: 57.9, lng: 12.5 }}
-      mapContainerClassName="map_container"
-    ></GoogleMap>
+    <GoogleMap zoom={12} center={center} mapContainerClassName="map_container">
+      <MarkerF position={hermansHus} label={"Hermans Hus! :D"}></MarkerF>
+    </GoogleMap>
   );
 };
 
