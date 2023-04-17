12fb06e10c71201338d0ef241383e2d3e0c53fb8
KingHermando <herman.noren@outlook.com>
2023-04-17 12:47:54 +0200

    Added MapEvents

---
+++ react_app/src/components/mapEvent/MapEvent.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Circle, MarkerF } from "@react-google-maps/api";
+
+const MapEvent = ({ lat, lng, radius, label }) => {
+  const [clicked, setClicked] = useState(false);
+  if (clicked)
+    return (
+      <Circle
+        center={{ lat: lat, lng: lng }}
+        radius={radius}
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+      ></Circle>
+    );
+  return (
+    <MarkerF
+      position={{ lat: lat, lng: lng }}
+      label={label}
+      onClick={() => {
+        setClicked(!clicked);
+      }}
+    ></MarkerF>
+  );
+};
+
+export default MapEvent;
