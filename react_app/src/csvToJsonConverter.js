b150e36d7be2389e097eedfd29edd83c8676334d
Gustav Gille <gustav.gille@icloud.com>
2023-04-19 11:58:51 +0200

    Added a folder to the fireValues.
    Also changed path in csvToJsonConverter.js.

--- react_app/src/csvToJsonConverter.js
+++ react_app/src/csvToJsonConverter.js
@@ -1,20 +1,19 @@
 
 //csvtojson needs to be installed (npm install cvstojson).
-const csvToJsonConverter = require('csvtojson');
+// Import the required modules
+const csvToJson = require('csvtojson');
 const fs = require('fs/promises');
 const path = require('path');
-console.log(__dirname);
-console.log('hi');
-// Define input and output file paths
-const inputFile = path.join(__dirname, '..', '..', 'Documents', 'fires2021.csv');
-const outputFile = path.join(__dirname, '..', '..', 'Documents', 'output.json');
 
+// Define input and output file paths
+const inputFile = path.join(__dirname, '..', '..', 'Documents', 'fireValues', 'fires2021.csv');
+const outputFile = path.join(__dirname, '..', '..', 'Documents', 'fireValues', 'output.json');
 
 // Function to convert CSV to JSON
 async function convertCsvToJson(input, output) {
     try {
         // Read the input CSV file and convert it to JSON
-        const jsonArray = await csvToJsonConverter().fromFile(input);
+        const jsonArray = await csvToJson().fromFile(input);
 
         // Write the JSON array to the output file
         await fs.writeFile(output, JSON.stringify(jsonArray, null, 2));
