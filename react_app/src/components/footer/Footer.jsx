9d827aa06aecd11ae223385f642f0932517272a9
Gille <93673070+it-like@users.noreply.github.com>
2023-04-24 15:25:30 +0200

    Added three seperate sliders with a scaling image, need to add the scaling image so that it does not oversize the Footer.jsx.

--- react_app/src/components/footer/Footer.jsx
+++ react_app/src/components/footer/Footer.jsx
@@ -1,11 +1,72 @@
-import * as React from "react";
-import './footer.css';
-
+import React, { useState } from "react";
+import Slider from "react-slider";
+import "./footer.css";
+import image from "../../img.png";
 
 function Footer() {
-  return (
-      <div className="footer"> Footer </div>
-  );
+    const [value1, setValue1] = useState(30);
+    const [value2, setValue2] = useState(25);
+    const [value3, setValue3] = useState(70);
+
+    const handleChange1 = (newValue) => {
+        setValue1(newValue);
+    };
+
+    const handleChange2 = (newValue) => {
+        setValue2(newValue);
+    };
+
+    const handleChange3 = (newValue) => {
+        setValue3(newValue);
+    };
+
+    // calculate the image size based on the second slider value
+    const imageSize = value2 * 2;
+
+    return (
+        <div className="footer">
+            <div className="slider-container">
+                <div className="slider1">
+                    <Slider
+                        value={value1}
+                        onChange={handleChange1}
+                        className="slider"
+                        thumbClassName="thumb"
+                        trackClassName="track"
+                        ariaLabel={"Volume 1"}
+                    />
+                </div>
+            </div>
+            <div>
+                {/* add an image that dynamically resizes */}
+                <img src={image} alt="image" style={{ width: imageSize }} />
+            </div>
+            <div className="slider-container">
+                <div className="slider2">
+                    <Slider
+                        value={value2}
+                        onChange={handleChange2}
+                        className="slider"
+                        thumbClassName="thumb"
+                        trackClassName="track"
+                        ariaLabel={"Volume 2"}
+                    />
+                </div>
+            </div>
+            <div className="slider-container">
+                <div className="slider3">
+                    <Slider
+                        value={value3}
+                        onChange={handleChange3}
+                        className="slider"
+                        thumbClassName="thumb"
+                        trackClassName="track"
+                        ariaLabel={"Volume 3"}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
