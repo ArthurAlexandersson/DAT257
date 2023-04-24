f1e1a5424827fc305b283c4e6322369559057cb4
Oscar Karbin <karbin.oscar2002@gmail.com>
2023-04-24 15:36:12 +0200

    added footer about us page

--- react_app/src/components/footer/Footer.jsx
+++ react_app/src/components/footer/Footer.jsx
@@ -4,7 +4,9 @@ import './footer.css';
 
 function Footer() {
   return (
-      <div className="footer"> Footer </div>
+      <div className="footer"> Footer
+        <div className={"about_us"}> <a href="https://www.youtube.com/watch?v=yEaKkBEbMrQ" target="_blank">About Us</a></div>
+      </div>
   );
 }
 
