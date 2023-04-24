be70f8b3ef621752e003ce462d29efd29092161d
jihad <jihad.mohammed95@gmail.com>
2023-04-24 17:25:24 +0200

    added button to slide th footer up and down

--- react_app/src/components/footer/Footer.jsx
+++ react_app/src/components/footer/Footer.jsx
@@ -1,10 +1,29 @@
 import * as React from "react";
 import './footer.css';
-
+import { useState } from "react";
 
 function Footer() {
+  let footer_size=60;
+  const [isOpen, setIsOpen] = useState(true);
+  const handleClose = () => {
+    const footer = document.querySelector('.footer');
+    if (isOpen) {
+      footer.classList.add('slide-down');
+      footer.classList.remove('slide-up');
+      setIsOpen(false);
+      footer_size=0;
+    } else {
+      footer.classList.add('slide-up');
+      footer.classList.remove('slide-down');
+      setIsOpen(true);
+      footer_size=60;
+    }
+  };
   return (
-      <div className="footer"> Footer </div>
+      <div className={`footer`}> Footer
+      <button className="button" onClick={handleClose}>Click me!</button>
+       </div>
+      
   );
 }
 
