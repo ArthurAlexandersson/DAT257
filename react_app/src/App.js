ff2265021d7e376580afb92ec2d3317dde1c033c
Oscar Karbin <karbin.oscar2002@gmail.com>
2023-05-02 13:49:39 +0200

    Changed structure of application in order to use routers. App.js --> Home.js

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext } from "react";
+import {useState, createContext } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -9,36 +9,22 @@ import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/errorPage";
 
-import fireData from "./fireValues/output.json";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 export const darkModeContext = createContext();
 export const leaderboardContext = createContext();
 
+
+
 function App() {
 
   const [isDarkModeState, setDarkModeState] = useState(false);
   const [leaderboardShown, setLeaderboardShown] = useState(false);
 
-/*  useEffect(() => {
-    const fetchEvents = async () => {
-      setLoadingData(true);
-      /!*
-      const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events");
-      const { events } = await res.json();
-      *!/
-
-      /!* setEventData(events); *!/
-      setEventData(fireData);
-      setLoadingData(false);
-    };
-
-    fetchEvents();
-  }, []);*/
-
   return (
     <div className="App">
+    <Router>
       <darkModeContext.Provider
         value={{
           isDarkModeState,
@@ -52,14 +38,15 @@ function App() {
           }}
         >
           <Header />
-          {!loadingData ? (
-            <InteractiveMap eventData={eventData} />
-          ) : (
-            <MapLoader />
-          )}
+          <Routes>
+            <Route path="DAT257/" element={<Home/>} />
+            <Route path="DAT257/aboutus" element={<AboutUs/>} />
+            <Route path="*" element={<ErrorPage/>}/>
+          </Routes>
           <Footer />
         </leaderboardContext.Provider>
       </darkModeContext.Provider>
+    </Router>
     </div>
   );
 }
